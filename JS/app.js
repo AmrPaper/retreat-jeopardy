217e15ac8ace7a236c75144f3f65b0fa9bbbc6e0
AmrPaper <amr.paper@proton.me>
2023-11-17 17:41:02 +0200

    Added question box pop up functionality alongside auto json file question info fetch to it, lots of work left on both of those tho

--- JS/app.js
+++ JS/app.js
@@ -5,6 +5,29 @@ fetch("/Data/questions.json")
         <p>Question text: ${data.text}</p>
         <p>Question ans: ${data.ans}</p>
         <p>Question value: ${data.value}</p>
+        <p>Question category: ${data.category}</p>
         `;
     })
     .catch(error => console.log("error fetching data: ", error));
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const cls = document.querySelector(".cls");
+const question = document.querySelectorAll(".question");
+
+const questionBox = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const back = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+question.forEach(questionElement => {
+    questionElement.addEventListener("click", questionBox);
+});
+overlay.addEventListener("click", back);
+cls.addEventListener("click", back);
+
